7f2dc97ec3178cb5282c09414a079d5c45be751e
sol92824 <sol92824@gmail.com>
2023-10-25 23:42:15 +0900

    pomodoro_screen 1번 Column 고정 데이터 + 디자인

--- lib/screens/pomodoro_screen.dart
+++ lib/screens/pomodoro_screen.dart
@@ -10,7 +10,82 @@ class PomodoroScreen extends StatelessWidget {
         title: const Text(
           'POMOTIMER',
         ),
-        backgroundColor: Colors.red.shade400,
+        backgroundColor: Colors.red.shade500,
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          color: Colors.red.shade500,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            vertical: 60,
+            horizontal: 80,
+          ),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    width: 100,
+                    height: 100,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(
+                        10,
+                      ),
+                    ),
+                    child: Center(
+                      child: Text(
+                        '04',
+                        style: TextStyle(
+                            color: Colors.red.shade400,
+                            fontSize: 50,
+                            fontWeight: FontWeight.w800),
+                      ),
+                    ),
+                  ),
+                  Column(
+                    children: [
+                      Icon(
+                        Icons.circle,
+                        color: Colors.red.shade200,
+                        size: 8,
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Icon(
+                        Icons.circle,
+                        color: Colors.red.shade200,
+                        size: 8,
+                      ),
+                    ],
+                  ),
+                  Container(
+                    width: 100,
+                    height: 100,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(
+                        10,
+                      ),
+                    ),
+                    child: Center(
+                      child: Text(
+                        '59',
+                        style: TextStyle(
+                            color: Colors.red.shade500,
+                            fontSize: 50,
+                            fontWeight: FontWeight.w800),
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
